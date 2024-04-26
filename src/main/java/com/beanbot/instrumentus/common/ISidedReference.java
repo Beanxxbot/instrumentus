4a9e619b75396014888a0b3e39b336798eaf31e7
Beanxxbot <btdrape@gmail.com>
2024-04-26 13:31:02 -0400

    Delete src/main directory

--- src/main/java/com/beanbot/instrumentus/common/ISidedReference.java
+++
@@ -1,9 +0,0 @@
-package com.beanbot.instrumentus.common;
-
-import net.minecraftforge.eventbus.api.IEventBus;
-
-@SuppressWarnings("unused")
-public interface ISidedReference {
-    void setup(final IEventBus mod, final IEventBus forge);
-
-}
